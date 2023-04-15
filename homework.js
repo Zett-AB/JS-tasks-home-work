3c41be5a5033b9b13cecacd516cc2e105ee95a31
Alexander <formysite.ru@yandex.ru>
2023-04-15 14:40:53 +0300

    not a css

--- homework.js
+++ homework.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 
 // let str='abcde';
 // alert(str[0]);
@@ -21,18 +21,94 @@
 
 // Работа с переменными
  
-let num=3; //1
-alert(num);
+// let num=3; //1
+// alert(num);
 
-let a=10;//2
-let b=2;
+// let a=10;//2
+// let b=2;
  
-alert(a+b);
-alert(a-b);
-alert(a*b);
-alert(a/b);
+// alert(a+b);
+// alert(a-b);
+// alert(a*b);
+// alert(a/b);
 
-let c = 15;//3
-let d = 2;
-let result=(c+d);
-alert(result);
+// let c = 15;//3
+// let d = 2;
+// let result=(c+d);
+// alert(result);
+
+// let a = 10; 
+// let b = 2; 
+// let c= 5; //4
+
+// alert(a+b+c);
+
+// let a = 17;//5
+// let b = 10;
+// let c = (a-b);
+// let d = 7;
+// let result = (c+d);
+// alert(result);
+
+//Работа со строками
+
+// let str = 'Привет, Мир!';//6
+// alert(str);
+
+// let str1 = 'Привет, ';//7
+// let str2 = 'Мир!';
+// alert(str1+str2);
+
+// let name = 'Александр';
+// alert('Привет, '+ name +'!');
+
+// let age = 51;
+// alert('Мне ' + age);
+
+//Работа с функцией prompt
+
+// let names = prompt('Напишите Ваше имя', '');//10
+// alert('Ваше имя ' + names);
+
+// let num = prompt('Напишите любое число', '');//11
+// alert(num*num);
+// alert(num**2);
+
+
+//Обращение к символам строки
+
+// let str = 'abcde';
+
+// alert(str[0]);
+// alert(str[2]);
+// alert(str[4]);
+
+// let num = '12345';
+// alert(num[0]*num[1]*num[2]*num[3]*num[4]);
+
+//Практика указать текущее время с помощью трех переменных час, минута секунда
+//создать скрипт, который считает количество секунд в часе, сутках и месяце.
+
+// alert(60*60);
+// alert(3600*24);
+// alert(86400*30);
+
+// let time = new Date();
+// time.getHours();
+// time.getMinutes();
+// time.getSeconds();
+// alert(time);
+
+// let hour = 'час';
+// let min = 'минута';
+// let sec = 'секунда';
+// alert(hour + ':'+ min + ':' + sec);
+
+// let num = 10;
+// num++;
+// num++;
+// num--;
+// alert(num);
+
+// let num = 50;
+// alert(num*2);
