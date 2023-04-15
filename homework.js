fbd3759c2d16494831b55a2fce1290ff49be0896
Alexander <formysite.ru@yandex.ru>
2023-04-15 12:47:51 +0300

    tasks stsrt

---
+++ homework.js
@@ -0,0 +1,38 @@
+'use strict'
+
+// let str='abcde';
+// alert(str[0]);
+// alert(str[1]);
+// alert(str[4]);
+
+// //task2
+
+// alert(60*60);
+
+// //task3
+// var num = 1;
+// num += 12;
+// num -= 14;
+// num *= 5;
+// num /= 7;
+// num++;
+// num--;
+// alert(num);
+
+// Работа с переменными
+ 
+let num=3; //1
+alert(num);
+
+let a=10;//2
+let b=2;
+ 
+alert(a+b);
+alert(a-b);
+alert(a*b);
+alert(a/b);
+
+let c = 15;//3
+let d = 2;
+let result=(c+d);
+alert(result);
